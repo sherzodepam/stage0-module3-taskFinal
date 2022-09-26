8179b3d8133a82f592128d654d9c4781c995e25e
Sherzod <sherzodd7707@gmail.com>
2022-09-26 23:50:08 +0500

    wow

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int x= 1111;
+        System.out.println(1+1+1+1);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.util.Scanner;
+
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please the temperature in Celcius");
+        double celcius=scanner.nextDouble();
+        double Fahrenheit= celcius*33.8;
+        System.out.println(celcius+" celcius equal to "+Fahrenheit);
     }
 }
